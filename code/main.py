ac3005696eb7cc6ef406bb60bfbc838842ff5585
n.shemelyov <shmelev507@gmail.com>
2024-04-24 21:06:58 +0200

    fields update

--- code/main.py
+++ code/main.py
@@ -1,11 +1,23 @@
+# from typing import Hashable
+from datetime import datetime
 class User:
-    def __init__(self, name) -> None:
-        self._id = None
-        self.name = name
-        self.admin = False
+    def __init__(self, name, surname, password, phone_number, nickname=None) -> None:
+        self._id: int = None
+        self.name: str = name
+        self.surname: str = surname
+        self.nickname: str = nickname
+        self.admin: bool = False
+        self.phone_number: int = phone_number
+        
+        self.password: str = self.__generate_password(password)
+        self.token = self.__generate_token()
+        
+    def __generate_password(self, password) -> str:
+        return str(hash(password))
     
     
-
+    def __generate_token(self) -> str:
+        return str(hash(datetime.now()))
 
 class Buyer(User):
     pass
@@ -18,3 +30,12 @@ class Admin(User):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(args, kwargs)
         self.admin = True
+        
+        
+user = User(
+    name="Nikita",
+    surname="Shmelyov",
+    phone_number=123,
+    password="password"
+)
+
